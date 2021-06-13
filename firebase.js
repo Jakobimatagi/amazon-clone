ddbb62338dd0731fcfb76bae31842a764e5fa469
Jakobi Matagi <jkmatagi@hotmail.com>
2021-06-13 14:28:18 -0600

    added login auth with google add to basket functionality using redux store next step remove from basket

---
+++ firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCjkxaMAa3ZkTrPPYcOkW8OpUiETE3KukQ",
+    authDomain: "clone-34929.firebaseapp.com",
+    projectId: "clone-34929",
+    storageBucket: "clone-34929.appspot.com",
+    messagingSenderId: "38135870677",
+    appId: "1:38135870677:web:5ed668f35344ac12acde5c"
+  };
---
+++ src/components/CheckoutProduct.js
@@ -0,0 +1,67 @@
+import Image from 'next/image'
+import React from 'react'
+import { StarIcon } from '@heroicons/react/solid';
+import Currency from 'react-currency-formatter';
+import { useDispatch } from 'react-redux';
+import { addToBasket, removeFromBasket } from "../slices/basketSlice"
+
+function CheckoutProduct({id, title, price, description, rating, category, image, hasPrime}) {
+
+    const dispatch = useDispatch()
+
+    const addItemToBasket = () => {
+        const product = {
+            id,
+            title,
+            price,
+            description,
+            rating,
+            category,
+            image,
+            hasPrime
+        }
+
+        //push items to redux store
+        dispatch(addToBasket(product))
+    }
+
+
+    return (
+        <div className="grid grid-cols-5 ">
+            <Image src={image} height={200} width={200} objectFit="contain" />
+            
+            {/* middle section */}
+            <div className="col-span-3 mx-5">
+                <p>{title}</p>
+                <div className="flex">
+                    {Array(rating)
+                    .fill()
+                    .map((_, i) => (
+                        <StarIcon key={i} className="h-5 text-yellow-500" />
+                    ))}
+                </div>
+                <p className="text-xs my-2 line-clamp-3">{description}</p>
+                <Currency 
+                    quantity={price}
+                    currency="USD"
+                />
+
+                {hasPrime && (
+                    <div className="flex items-center space-x-2">
+                        <img loading="lazy" className="w-12" src="https://links.papareact.com/fdw" alt="amazon prime" />
+                        <p className="text-xs text-gray-500">Free Next-Day Delivery</p>
+                    </div>
+                )}
+            </div>
+            
+            {/* add and remove button */}
+            <div className="flex flex-col space-y-2 my-auto justify-self-end">
+                <button className="button" onClick={addItemToBasket}>Add To Basket</button>
+                <button className="button">Remove From Basket</button>
+            </div>
+        </div>
+
+    )
+}
+
+export default CheckoutProduct
