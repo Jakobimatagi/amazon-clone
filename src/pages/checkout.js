d5094b2519a4a43257520abd67727ca28aff52a9
Jakobi Matagi <jkmatagi@hotmail.com>
2021-06-16 12:40:40 -0600

    added prooced to checkout section

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -3,13 +3,15 @@ import Header from '../components/Header'
 import CheckoutProduct from '../components/CheckoutProduct'
 import Image from 'next/image'
 import { useSelector } from 'react-redux'
-import { selectItems } from '../slices/basketSlice'
-
+import { selectItems, selectTotal } from '../slices/basketSlice'
+import Currency from 'react-currency-formatter'
+import { useSession } from 'next-auth/client'
 
 function Checkout() {
 
     const items = useSelector(selectItems)
-
+    const total = useSelector(selectTotal)
+    const [session] = useSession()
     return (
         <div className="bg-gray-100">
             <Header />
@@ -26,7 +28,7 @@ function Checkout() {
 
                 </div>
 
-                    {/* rigt side */}
+                    
                 <div className="flex flex-col p-5 space-y-10 bg-white">
                     <h1 className="text-3xl border-b pb-4">{items.length === 0 ? "Your Amazon Basket Is Empty" : "Your Amazon Basket"}</h1>
 
@@ -43,7 +45,26 @@ function Checkout() {
                         hasPrime={item.hasPrime}
                       />
                     ))}
+                {/* rigt side */}
+                    <div className="flex flex-col bg-white p-10 shadow-md">
+                        {items.length > 0 && (
+                            <>
+                                <h2 className="whitespace-nowrap">Subtotal ({items.length} items):
+                                <span>
+                                    <Currency
+                                    quantity={total} currency="USD"
+                                     />
+                                </span>
 
+
+                                </h2>
+
+                                <button disabled={!session} className={`button mt-2 ${!session && 'from-gray-300 to-gray-500 border-gray-200 text-gray-300'}`}>
+                                    {!session ? 'Sign In to Checkout' : 'Proceed To Checkout' }
+                                </button>
+                            </>
+                        )}
+                    </div>
                 </div>
             </main>
         </div>
